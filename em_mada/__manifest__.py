711aecb2350eb5a6bcd97d454e8d4de947ee3230
MadaOdoo <103079439+MadaOdoo@users.noreply.github.com>
2022-04-26 14:19:29 -0500

    MADA - Extension Module v15.0.0.0.1

---
+++ em_mada/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MADA - Extension Module",
+
+    'summary': """MADA - Extension Module""",
+
+    'description': """MADA - Extension Module""",
+    'author': "Alfonso Gonzalez",
+    'website': "https://ntropy.tech",
+    'category': 'Customizations',
+    'version': '15.0.0.0.1',
+    'license': "AGPL-3",
+    'sequence': "-90",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        # Security
+        # Wizard
+        # Views
+        # Data Sample
+
+    ],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
