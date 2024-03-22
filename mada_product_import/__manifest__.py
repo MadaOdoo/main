bcb2ca110519d5582cdd9ac24a98dce7a6dbcefa
DesarrolloPtree <61703526+DesarrolloPtree@users.noreply.github.com>
2024-03-22 17:45:17 -0500

    Fix

--- mada_product_import/__manifest__.py
+++ mada_product_import/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Mada Product Import',
-    'version': '0.2',
+    'version': '0.0.2',
     'category': 'sale',
     'description': """
     Mada Product Import
